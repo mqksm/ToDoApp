ef2343bb75b3456fa2998e9c310209f697c4571c
Mqksm <itismyfriend@gmail.com>
2020-05-03 17:20:57 +0300

    Initial Commit

---
+++ ToDoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Maks on 03.05.2020.
+//  Copyright © 2020 Maxim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
